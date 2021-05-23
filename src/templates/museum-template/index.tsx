1e09c54eefe05e0d5a8c827e17b718b141f6edf9
Lucas Nóbrega <lucas@pop-os.localdomain>
2021-05-22 22:17:39 -0300

    Criando o componente de rodape (footer)

--- src/templates/museum-template/index.tsx
+++ src/templates/museum-template/index.tsx
@@ -1,12 +1,13 @@
 // Components
+import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
 
 export default function MuseumTemplate({ children }) {
   return (
     <div className="app">
       <Navbar />
-      <main>{children}</main>
-      <footer>Footer</footer>
+      <main style={{ height: "100px" }}>{children}</main>
+      <Footer />
     </div>
   );
 }
