8d6985157c2acde1f70190a84cdb7dcae9110868
João Pedro Araújo <joao.pedroasz00@gmail.com>
2021-06-07 21:02:48 -0300

    Criando funionalidade de tirar zoom da imagem no componente de exibição quando passar o mouse

--- src/components/exhibition-card/index.tsx
+++ src/components/exhibition-card/index.tsx
@@ -14,16 +14,24 @@ export default function ExhibitionCard(props: IExhibitionCardProps) {
       <Link href={pathToExhibition}>
         <div className={styles.card}>
           <div
-            className={styles.information}
+            className={styles.cardMainContent}
             style={{
-              background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 42.71%, #121212 100%), url("https://i.pinimg.com/originals/ca/4b/91/ca4b9102f39d3b1cfa1756f93ad175c7.jpg"), #000000`,
+              backgroundImage:
+                'url("https://i.pinimg.com/originals/ca/4b/91/ca4b9102f39d3b1cfa1756f93ad175c7.jpg")',
             }}
           >
-            <h1 className={styles.title}>Lorem Ipsum</h1>
-            <p className={styles.description}>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam aliquid vel quibusdam
-              eius assumenda?
-            </p>
+            <div
+              className={styles.information}
+              style={{
+                background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 42.71%, #121212 100%)`,
+              }}
+            >
+              <h1 className={styles.title}>Lorem Ipsum</h1>
+              <p className={styles.description}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam aliquid vel quibusdam
+                eius assumenda?
+              </p>
+            </div>
           </div>
           <div className={styles.dateAndView}>
             <b>04 de mai. de 2021</b>
